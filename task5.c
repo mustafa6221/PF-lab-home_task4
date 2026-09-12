b47cc0e4208cbc73aa382ea64ab135c9f2431e24
mustafa6221 <mustafaishaq2007@gmail.com>
2026-09-12 08:56:17 +0500

    Update task5.c

--- task5.c
+++ task5.c
@@ -1,7 +1,4 @@
-// Write a C program that reads the number of electricity units consumed in a month and computes the bill using
-// the following slab rates, implemented with an if-else-if-else ladder: first 100 units @ Rs. 5/unit, next 100 units
-// (101-200) @ Rs. 8/unit, next 200 units (201-400) @ Rs. 12/unit, and any units above 400 @ Rs. 15/unit.
-#include <stdio.h>
+
 int main()
 {
     int units;
